176fa67e5d1fa616ddafafcbec04d26c1a54821a
Mahesh <maheshzalte2000@gmail.com>
2024-10-03 23:06:55 +0530

    Structure

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,16 +3,16 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav className=" flex gap-1 w-full h-10 items-center">
+    <nav className=" flex gap-1 w-full h-10 p-3 items-center">
       <ul className="flex gap-2 ">
         <li>
-          <Link href="/news">Home</Link>
+          <Link href="/">Home</Link>
         </li>
         <li>
-          <Link href="/news">Home</Link>
+          <Link href="/news">News</Link>
         </li>
         <li>
-          <Link href="/news">Home</Link>
+          <Link href="/news/newsdetails">NewsDetails</Link>
         </li>
         <li>
           <Link href="/news">Home</Link>
